567ca57f09f4c68ad9c08b79e6146ad261bbaf36
dariovargaos <dario.varga2012@gmail.com>
2024-03-09 22:51:10 +0100

    Add useFetchCategories custom hook for fetching product categories

---
+++ shop-sphere/src/hooks/useFetchCategories.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchCategories = async () => {
+  const response = await fetch("https://dummyjson.com/products/categories");
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+};
+
+export const useFetchCategories = () => {
+  return useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+};
